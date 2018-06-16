9e0ad323d51479a6024601633467bac798ebef5d
asakon <kondoasako@gmail.com>
2018-06-16 10:04:00 +0900

    Channel一覧の定義

--- components/ChannelList.tsx
+++ components/ChannelList.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
+import { Menu, Icon } from 'semantic-ui-react';
 
 export class ChannelList extends React.Component<{}, {}> {
 
   public render() {
     return (
-      <div />
+      <Menu inverted vertical fixed={'left'}>
+        <Menu.Item>
+        Channels
+          <Menu.Menu>
+            <Menu.Item>
+            general
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu.Item>
+      </Menu>
     );
   }
 }
