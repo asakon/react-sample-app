c1fccfc8107797881ce4512a81eacdeee1ed122b
asakon <kondoasako@gmail.com>
2018-06-16 10:07:38 +0900

    チャンネル一覧を配列で定義して、map()で取り出す

--- components/ChannelList.tsx
+++ components/ChannelList.tsx
@@ -4,14 +4,19 @@ import { Menu, Icon } from 'semantic-ui-react';
 export class ChannelList extends React.Component<{}, {}> {
 
   public render() {
+    // 簡略化のため、チャンネル一覧をここで定義
+    const channels = ['general', 'ramdom'];
+
     return (
       <Menu inverted vertical fixed={'left'}>
         <Menu.Item>
         Channels
           <Menu.Menu>
-            <Menu.Item>
-            general
-            </Menu.Item>
+            {channels.map(channel =>
+              <Menu.Item key ={channel}>
+              {channel}
+              </Menu.Item>
+            )}
           </Menu.Menu>
         </Menu.Item>
       </Menu>
