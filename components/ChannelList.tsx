ad5228848afb83777309ab1bf099acd3e3ae9653
asakon <kondoasako@gmail.com>
2018-06-18 12:21:10 +0900

    Functional Component

--- components/ChannelList.tsx
+++ components/ChannelList.tsx
@@ -7,7 +7,25 @@ const channels = ['general', 'random'];
 export const ChannelList = () => {
   return (
     <Menu inverted vertical fixed={'left'}>
-
+      <Menu.Item as={Link} to={'/'}>
+        Home
+        <Icon name='home' />
+      </Menu.Item>
+      <Menu.Item>
+        Channels
+        <Icon name='list' />
+        <Menu.Menu>
+          {channels.map(channel =>
+              <Menu.Item
+                key={channel}
+                name={channel}
+                as={NavLink}
+                to={{ pathname: `/channels/${channel}` }}>
+                {channel}
+              </Menu.Item>
+          )}
+        </Menu.Menu>
+      </Menu.Item>
     </Menu>
-  )
-}
+  );
+};
