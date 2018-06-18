e77b7788cfcc7566771dffb72edff82e277213da
asakon <kondoasako@gmail.com>
2018-06-18 12:11:36 +0900

    classではなく、Functional Componentとして定義 start

--- components/ChannelList.tsx
+++ components/ChannelList.tsx
@@ -2,28 +2,12 @@ import * as React from 'react';
 import { Menu, Icon } from 'semantic-ui-react';
 import { Link, NavLink } from 'react-router-dom';
 
-export class ChannelList extends React.Component<{}, {}> {
+const channels = ['general', 'random'];
 
-  public render() {
-    // 簡略化のため、チャンネル一覧をここで定義
-    const channels = ['general', 'ramdom'];
+export const ChannelList = () => {
+  return (
+    <Menu inverted vertical fixed={'left'}>
 
-    return (
-      <Menu inverted vertical fixed={'left'}>
-        <Menu.Item>
-        Channels
-          <Menu.Menu>
-            {channels.map(channel =>
-              <Menu.Item
-                key={channel}
-                as={NavLink}
-                to={{ pathname: `/channels/${channel}` }}>
-              {channel}
-              </Menu.Item>
-            )}
-          </Menu.Menu>
-        </Menu.Item>
-      </Menu>
-    );
-  }
+    </Menu>
+  )
 }
