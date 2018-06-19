304fdc1e48d8fe3dec986aaa370a11beace6dd28
asakon <kondoasako@gmail.com>
2018-06-19 13:42:39 +0900

    cacheスペルミスを修正

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports  = [{
-  cashe: true,
+  cache: true,
   devtool: 'source-map',
   devServer: {
     historyApiFallback: true,
