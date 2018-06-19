58fba06eb92173e3647ca7c3f7314da9aa3fa0fd
asakon <kondoasako@gmail.com>
2018-06-19 12:05:59 +0900

    パッケージのインポート

---
+++ Routes.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { Switch } from 'react-router';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { ChannelList } from './components';
+import { Container } from 'semantic-ui-react';
